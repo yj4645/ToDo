693137f0dd39cafc33c25d82549fc6382149f055
yj4645 <69782652+yj4645@users.noreply.github.com>
2020-10-19 01:28:24 +0900

    task一覧表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   root to: 'tasks#index'
   # # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tasks, only: [:index, :new, :create]
+  resources :tasks do
   end
+  end
+  
 
